41fcc4a0fcff439dab915003c81cbdc9821fab8d
rickmurdock <rick.murdock@live.com>
2017-07-06 18:44:46 -0500

    Fixed body-parser logic to allow form input for posting.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mustacheExpress = require('mustache-express');
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 const Car = require("./models/Car");
@@ -7,7 +8,15 @@ const app = express();
 const port = process.env.port || 8005;
 const dbURL = "mongodb://localhost:27017/classiCars";
 
+// RENDER ENGINE
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache")
+
+app.use(express.static('public'));
+app.use("/", express.static(__dirname + "/views"));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 mongoose.connect(dbURL).then(function(err, db) {
   if (err) {
@@ -16,14 +25,21 @@ mongoose.connect(dbURL).then(function(err, db) {
   console.log("connected to classicCar DB.");
 });
 
-app.get("/cars", (req, res) => {
+app.get("/", (req, res) => {
   Car.find()
   .then(foundCars=>{
-    res.send(foundCars);
+    //  res.send(foundCars);
+
+    res.render("index", { autos: foundCars});
   })
   .catch(err => {
     res.status(500).send(err);
   });
+});
+
+
+app.get("/addCar", (req, res) => {
+  res.render('addCar');
 });
 
 app.get("/cars/:id", (req, res) => {
@@ -37,14 +53,26 @@ app.get("/cars/:id", (req, res) => {
     });
 });
 
-app.post("/cars", (req, res) => {
+app.post("/addCar", (req, res) => {
   let carData = req.body;
+  console.log('CAR DATA ====', carData);
   let newCar = new Car(carData);
   console.log("newCar: ", newCar);
   newCar
     .save()
     .then(savedCar => {
-      res.send(savedCar);
+      // res.send(savedCar);
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+app.put("/cars/:id", (req, res) => {
+  Car.updateOne({_id: req.params.id}, req.body)
+  .then(updatedCar=> {
+    res.send(updatedCar);
     })
     .catch(err => {
       res.status(500).send(err);
